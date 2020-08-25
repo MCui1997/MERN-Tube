afb56d81aa5775fa6845c359aca6d1c4b06e5eb1
Michael Cui <cuimichael500@gmail.com>
2020-08-24 19:13:59 -0500

    heroku

--- server.js
+++ server.js
@@ -14,28 +14,30 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-  });
-}
-
 const mongoURI =
   "mongodb+srv://msway1997:HpY17fD6B7EsrpaM@cluster0.5ldjj.mongodb.net/<main>?retryWrites=true&w=majority";
 
 mongoose
-  .connect(process.env.MONGOLAB_URI || mongoURI, {
+  .connect(process.env.MONGODB_URI || mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+//Routes
 var Users = require("./routes/Users");
 
 app.use("/users", Users);
+
+//Step 3
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, function () {
   console.log("Server is running on port: " + port);
